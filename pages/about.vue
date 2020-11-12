1fa9bac2e82e8d35c85cb51f0d07e470403a64c3
Arpit <arpit.yadav1912@gmail.com>
2020-11-12 23:55:15 +0530

    Added basic home page and projects page

---
+++ pages/about.vue
@@ -0,0 +1,8 @@
+<template>
+    <div class="mx-auto">
+        <div class="mt-24">
+            <p>ZAWARUDO</p>
+        </div>
+    </div>
+</template>
+
---
+++ pages/projects.vue
@@ -0,0 +1,99 @@
+<template>
+    <div class="grid grid-cols-11 gap-0">
+        <div class="col-start-4 col-end-9 mx-12">
+            <div class="border-b flex pb-10">
+                <img src="/_nuxt/assets/pic3.png" alt="" class="border rounded-full w-32">
+                <div class="flex flex-col items-start justify-center ml-8">
+                    <h2 class="text-3xl">Arpit&nbsp;Yadav</h2>
+                    <p class="text-gray-600 text-lg">Software&nbsp;Developer</p>
+                </div>
+            </div>
+
+            <div>
+                <h3>Projects</h3>
+                <p>6 Projects</p>
+
+                <h2 class="mt-4 text-2xl text-gray-900 underline">Cyberbub</h2>
+                <ul class="border-b list-disc mono mt-4 pb-6 text-gray-800">
+                    <img src="../assets/cyberbub.png" alt="Cyberbub image" class="w-full my-8 shadow-xs">
+                    <li class="mt-2">Created with Laravel, Vue and Tailwind (and many other services like Algolia, Pusher, etc)</li>
+                    <li class="mt-2">Backend is fully backed with TDD (PHPUnit)</li>
+                    <li class="mt-2">A personal project that lets you to find your coding partner</li>
+                    <li class="mt-2">You can chat, manage tasks and do many things in real time</li>
+
+                    <a href="http://www.cyberbub.team" target="_blank" class="text-sm text-gray-800 hover:text-gray-700 hover:underline">View Site</a>
+                </ul>
+                <h2 class="mt-4 text-2xl text-gray-900 underline">Forum</h2>
+                <ul class="border-b list-disc mono mt-4 pb-6 text-gray-800">
+                    <img src="../assets/forum.png" alt="Forum image" class="w-full my-8 shadow-xs">
+
+                    <li class="mt-2">Created with Laravel, Vue and Bootstrap</li>
+                    <li class="mt-2">Backend is fully backed with TDD (PHPUnit)</li>
+                    <li class="mt-2">Based on video series by Jeffrey Way <a href="#">(Laracasts)</a></li>
+
+                    <a href="#" class="text-sm text-gray-800 hover:text-gray-700 hover:underline">View Source Code</a>
+                </ul>
+
+                <h2 class="mt-4 text-2xl text-gray-900 underline">MoviesApp</h2>
+                <ul class="border-b list-disc mono mt-4 pb-6 text-gray-800">
+                    <img src="../assets/movie.png" alt="Movie image" class="w-full my-8 shadow-xs">
+
+                    <li class="mt-2">Created with Laravel, AlpineJS, Livewire, Tailwind CSS and The Movie DB REST API</li>
+                    <li class="mt-2">Test Driven Development (PHPUnit)</li>
+                    <li class="mt-2">A project based on </li>
+
+                    <a href="#" class="text-sm text-gray-800 hover:text-gray-700 hover:underline">View Source Code</a>
+                </ul>
+
+                <h2 class="mt-4 text-2xl text-gray-900 underline">Birdboard</h2>
+                <ul class="border-b list-disc mono mt-4 pb-6 text-gray-800">
+                    <img src="../assets/birdboard.png" alt="Birdboard image" class="w-full my-8 shadow-xs">
+
+                    <li class="mt-2">Created with Laravel, AlpineJS, Livewire, Tailwind CSS and The Movie DB REST API</li>
+                    <li class="mt-2">Test Driven Development (PHPUnit)</li>
+                    <li class="mt-2">A project based on </li>
+
+                    <a href="#" class="text-sm text-gray-800 hover:text-gray-700 hover:underline">View Source Code</a>
+                </ul>
+
+                <h2 class="mt-4 text-2xl text-gray-900 underline">Reddit-Clone</h2>
+                <ul class="border-b list-disc mono mt-4 pb-6 text-gray-800">
+                    <img src="../assets/reddit.png" alt="Reddit-clone image" class="w-full my-8 shadow-xs">
+
+                    <li class="mt-2">Created with Laravel, Vue.js and Tailwindcss</li>
+                    <li class="mt-2">Backend is fully backed with TDD (PHPUnit)</li>
+                    <li class="mt-2">Users can create a thread and vote to other threads in real time</li>
+                    <li class="mt-2">Comments and Replies in a thread will be posted in real time</li>
+                    <li class="mt-2">Created just for fun and to test my knowledge, also does not have the all the reddit functions (ofcourse!)</li>
+
+                    <a href="#" class="text-sm text-gray-800 hover:text-gray-700 hover:underline">View Source Code</a>
+                </ul>
+
+                <h2 class="mt-4 text-2xl text-gray-900 underline">ShoppingCart</h2>
+                <ul class="border-b list-disc mono mt-4 pb-6 text-gray-800">
+                    <img src="../assets/cart.png" alt="Shopping-cart image" class="w-full my-8 shadow-xs">
+
+                    <li class="mt-2">Created with Laravel, Vue.js and Tailwindcss</li>
+                    <li class="mt-2">Backend is fully backed with TDD (PHPUnit)</li>
+                    <li class="mt-2">Users can create a thread and vote to other threads in real time</li>
+                    <li class="mt-2">Comments and Replies in a thread will be posted in real time</li>
+                    <li class="mt-2">Created just for fun and to test my knowledge, also does not have the all the reddit functions (ofcourse!)</li>
+
+                    <a href="#" class="text-sm text-gray-800 hover:text-gray-700 hover:underline">View Source Code</a>
+                </ul>
+
+                <h2 class="mt-4 text-2xl text-gray-900 underline">Lincher - A social network clone</h2>
+                <ul class="border-b list-disc mono mt-4 pb-6 text-gray-800">
+                    <img src="../assets/social.png" alt="Shopping-cart image" class="w-full my-8 shadow-xs">
+
+                    <li class="mt-2">Created with Laravel, Vue and Tailwind (and many other services like Algolia, Pusher, etc)</li>
+                    <li class="mt-2">Backend is fully backed with TDD (PHPUnit)</li>
+                    <li class="mt-2">A personal project that lets you to find your coding partner</li>
+                    <li class="mt-2">You can chat, manage tasks and do many things in real time</li>
+
+                    <a href="http://www.cyberbub.team" target="_blank" class="text-sm text-gray-800 hover:text-gray-700 hover:underline">View Site</a>
+                </ul>
+            </div>
+        </div>
+    </div>
+</template>
