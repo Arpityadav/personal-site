a867fd31748923c590dde1f39ab1f87378426097
Arpit <arpit.yadav1912@gmail.com>
2021-06-18 18:56:09 +0530

    foo

--- pages/about.vue
+++ pages/about.vue
@@ -1,7 +1,18 @@
 <template>
-    <div class="mx-auto">
-        <div class="mt-24">
-            <p>ZAWARUDO</p>
+    <div class="grid grid-cols-11 gap-0">
+        <div class="col-start-4 col-end-9 mx-12">
+            <div class="border-b flex pb-10">
+                <img src="/_nuxt/assets/pic3.png" alt="" class="border rounded-full w-16">
+                <div class="flex flex-col items-start justify-center ml-8">
+                    <h2 class="text-3xl">Arpit&nbsp;Yadav</h2>
+                    <p class="text-gray-600 text-lg">Software&nbsp;Developer</p>
+                </div>
+            </div>
+            <div class="mt-10">
+                <p class="text-gray-800 text-lg mono">Hey, I’m an independent web & software developer from Dublin, Ireland. I work primarily with <span class="text-pink-400">Ember.js</span> and <span class="text-pink-400">Django</span>. I'm available on freelance basis.</p>
+
+                <a href="mailto:#">hey@arpityadav.com</a><br/>
+            </div>
         </div>
     </div>
 </template>
