bda0be1fa5d7d2d09e5b74ce28100d610d1f451c
Arpityadav <arpit.yadav1912@gmail.com>
2021-10-04 22:00:06 +0530

    Added linkedin link

--- pages/about.vue
+++ pages/about.vue
@@ -3,7 +3,7 @@
         <div class="md:col-start-4 md:col-end-9 mx-12">
             <p class="text-gray-800 text-lg mono mb-4">Hey, I’m an independent web & software developer from Noida, India. I work primarily with <span class="text-primary">Laravel</span> and <span class="text-primary">Vue.JS</span>. I'm available on freelance basis.</p>
             <p>Download Resume: <a class="text-primary" href="/resume_upload.doc" target="_blank">Resume.doc</a></p>
-            <p>My <a href="https://www.github.com/arpityadav" target="_blank" class="text-primary">Github</a></p>
+            <p><a href="https://www.github.com/arpityadav" target="_blank" class="text-primary">Github</a> | <a href="https://www.linkedin.com/in/arpit-yadav-b375a4134/" target="_blank" class="text-primary">Linkedin</a></p>
             
             <a href="mailto:hey_arpit@outlook.com" class="text-primary">hey_arpit@outlook.com</a><br/>
         </div>
